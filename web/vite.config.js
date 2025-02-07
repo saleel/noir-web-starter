ac2213b94107c9fb6a74f8f9fa503014a652ee60
saleel <saleel@saleel.xyz>
2025-02-07 15:46:57 +0000

    webpack

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,4 +4,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js']
   }
-}); 
+});
---
+++ web/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 5173, // Using the same port as Vite for consistency
+    hot: true,
+  },
+  resolve: {
+    fallback: {
+      "fs": false,
+      "path": false,
+      "crypto": false
+    }
+  }
+}; 
